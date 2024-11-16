7cd1a26c112e68d6f58e5735bba5e579ef7dad96
Gourav Prusty <gouravprusty1432@gmail.com>
2024-11-17 01:39:43 +0530

    Fully responsive

--- js/script.js
+++ js/script.js
@@ -24,3 +24,15 @@ navBtns.forEach((btn) => {
 document.querySelector("#contact_button").addEventListener("click", () => {
     document.querySelector('[data-target="contact"]').classList.add("active");
 })
+// let total_count;
+// if(window.innerWidth < "600px"){
+//     total_count = Math.ceil(window.innerWidth/60) * Math.ceil(window.innerHeight/60);
+// }else{
+//     total_count = Math.ceil(window.innerWidth/100) * Math.ceil(window.innerHeight/100);
+// };
+// const container = document.querySelector(".background__container");
+// for(let i = 0; i < total_count + 50; i++){
+//     const newBox = document.createElement("div");
+//     newBox.classList.add("box");
+//     container.appendChild(newBox);
+// }
